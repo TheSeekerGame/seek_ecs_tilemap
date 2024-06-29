1b150195572664aea83875ddc9dfdd52af70c61c
destravous <destravous@gmail.com>
2024-06-28 21:19:53 -0400

    tests

--- examples/basic.rs
+++ examples/basic.rs
@@ -22,6 +22,7 @@ fn main() {
     app.add_plugins(TilemapPlugin);
     app.add_systems(Startup, setup);
     app.add_systems(Update, updates);
+    app.add_systems(Update, updates_entire_map_pos);
     app.add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin::default());
     app.run();
@@ -40,41 +41,43 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
          ..default()
      });*/
 
-    let width = 50;
-    let height = 50;
-    let e_tilemap = commands
-        .spawn(TilemapBundle {
-            grid_size: TilemapGridSize::new(17.0, 17.0),
-            map_type: TilemapType::Square,
-            size: TilemapSize::new(width, height),
-            spacing: Default::default(),
-            storage: Default::default(),
-            texture: TilesetTexture::Single(texture_handle),
-            tile_size: TilemapTileSize::new(16.0, 16.0),
-            chunks: TilemapChunks::default(),
-            //transform: Transform::from_scale(Vec3::splat(0.25)),
-            transform: Transform::from_scale(Vec3::splat(0.5)),
-            global_transform: Default::default(),
-            visibility: Default::default(),
-            inherited_visibility: Default::default(),
-            view_visibility: Default::default(),
-        })
-        .id();
-    for y in 0..height {
-        for x in 0..width {
-            let idx = (x % 24) + ((y % 16) * width);
-            //println!("{}", idx);
-            commands.spawn(TileBundle {
-                position: TilePos::new(x, y),
-                texture_index: TileTextureIndex(idx),
-                tilemap_id: TilemapId(e_tilemap),
-                //visible: TileVisible(y % 2 == 0 || x % 2 == 0),
-                visible: TileVisible::default(),
-                flip: TileFlip { x: false, y: false, d: false},
-                //color: TileColor(Srgba::rgb_u8(((x *25) % 256) as u8, ((y * 25) % 256) as u8, 0).into()),
-                color: TileColor(Srgba::WHITE.into()),
-                old_position: default(),
-            });
+    for i in 1..9u32 {
+        let width = 50 * i;
+        let height = random::<u8>() as u32 * i;
+        let e_tilemap = commands
+            .spawn(TilemapBundle {
+                grid_size: TilemapGridSize::new(17.0, 17.0),
+                map_type: TilemapType::Square,
+                size: TilemapSize::new(width, height),
+                spacing: Default::default(),
+                storage: Default::default(),
+                texture: TilesetTexture::Single(texture_handle.clone()),
+                tile_size: TilemapTileSize::new(16.0, 16.0),
+                chunks: TilemapChunks::default(),
+                //transform: Transform::from_scale(Vec3::splat(0.25)),
+                transform: Transform::from_scale(Vec3::splat(0.5 / (i as f32))),
+                global_transform: Default::default(),
+                visibility: Default::default(),
+                inherited_visibility: Default::default(),
+                view_visibility: Default::default(),
+            })
+            .id();
+        for y in 0..height {
+            for x in 0..width {
+                let idx = (x % 24) + ((y % 16) * width);
+                //println!("{}", idx);
+                commands.spawn(TileBundle {
+                    position: TilePos::new(x, y),
+                    texture_index: TileTextureIndex(idx),
+                    tilemap_id: TilemapId(e_tilemap),
+                    //visible: TileVisible(y % 2 == 0 || x % 2 == 0),
+                    visible: TileVisible::default(),
+                    flip: TileFlip { x: false, y: false, d: false },
+                    //color: TileColor(Srgba::rgb_u8(((x *25) % 256) as u8, ((y * 25) % 256) as u8, 0).into()),
+                    color: TileColor(Srgba::WHITE.into()),
+                    old_position: default(),
+                });
+            }
         }
     }
 }
@@ -89,19 +92,31 @@ pub fn updates(mut q_tile: Query<
         &mut TileVisible,
     )>, mut idx: Local<usize>,
 ) {
-    let Some((id, pos, mut color, flip, mut visible)) = q_tile.iter_mut().skip(*idx).next() else {
-        *idx = 0;
-        return
-    };
-    return;
-    *color = TileColor(Srgba::new(
-        random::<f32>(),
-        random::<f32>(),
-        random::<f32>(),
-        1.0,
-    ).into());
+    for i in 0..10 {
+        let Some((id, pos, mut color, flip, mut visible)) = q_tile.iter_mut().skip(*idx).next() else {
+            *idx = 0;
+            return
+        };
 
-    visible.0 = random::<bool>();
+        *color = TileColor(Srgba::new(
+            random::<f32>(),
+            random::<f32>(),
+            random::<f32>(),
+            1.0,
+        ).into());
 
-    *idx += 1
+        visible.0 = random::<bool>();
+
+        *idx += 1
+    }
+}
+
+pub fn updates_entire_map_pos(
+    mut q_map: Query<&mut Transform, With<TilemapType>>,
+    time: Res<Time>,
+) {
+    for (i, mut map) in q_map.iter_mut().enumerate() {
+        let time = time.elapsed_seconds() * (1.1 * i as f32) * 0.1;
+        map.translation = Vec3::new(time.sin() * 100.0, time.cos() * 100.0, 0.0);
+    }
 }
